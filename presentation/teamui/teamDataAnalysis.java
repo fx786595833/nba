cc0aaf9512f3df18083e46c66a2064b600e348e2
fx786595833 <786595833@qq.com>
2015-04-01 15:24:26 +0800

    界面修改

--- presentation/teamui/teamDataAnalysis.java
+++ presentation/teamui/teamDataAnalysis.java
@@ -5,10 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import javax.swing.*;
 import javax.swing.table.*;
 
+import playerbl.MemberLogic;
 import teambl.teamsManager;
 import vo.teamVO;
 
@@ -21,14 +23,22 @@ public class teamDataAnalysis{
 			"犯规数","比赛得分","投篮命中率","三分命中率","罚球命中率","胜率","进攻回合","进攻效率","防守效率",
 			"篮板效率","抢断效率","助攻率"};
 //	private String[][] data = {};
-	DefaultTableModel tableModel = new DefaultTableModel(0,28);
-	JTable table = new JTable(tableModel);
-	JScrollPane jsp = new JScrollPane(table);
-	JButton button = new JButton("返回");
 	
 	public void go(String n,Icon i) throws IOException{
 		teamsManager t = new teamsManager();
 		teamDataAnalysis td = new teamDataAnalysis();
+		
+		Object[][] data=t.getSingleTeamData(n);
+		
+		MyTableModel tableModel = new MyTableModel(data,team);
+		TableRowSorter<MyTableModel> sorter = new TableRowSorter<MyTableModel>(tableModel);
+		JTable table = new JTable(tableModel);
+		JScrollPane jsp = new JScrollPane(table);
+		table.setAutoCreateRowSorter(true); 
+		table.setRowSorter(sorter); 
+		table.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
+		
+		JButton button = new JButton("返回");
 		
 		int wide = frame.getToolkit().getScreenSize().width;      //获取屏幕长宽
 		int high = frame.getToolkit().getScreenSize().height;
@@ -41,20 +51,13 @@ public class teamDataAnalysis{
 		
 		button.addActionListener(new returnListener());
 		
-		tableModel.setColumnIdentifiers(team);
-		frame.setSize(wide,high);
+		
+		table.setFillsViewportHeight(true);
+		frame.setSize(wide/2+100,high/2+200);
+		frame.setLocation(wide/4,high/6);
 		frame.setLayout(new BorderLayout());
 		frame.setResizable(false);
-		frame.setLocation(0,0);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		ArrayList<teamVO> list = t.getSingleTeamData(n);
-		for(int i1=0;i1<list.size();i1++){
-			String[] temp = new String[28];
-			temp = td.getRow(list.get(i1));
-			tableModel.insertRow(i1, temp);
-		} 
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		frame.add(jsp,BorderLayout.CENTER);
 		frame.add(panel,BorderLayout.NORTH);
 		frame.setVisible(true);
@@ -62,40 +65,6 @@ public class teamDataAnalysis{
 		
 	}
 
-	private String[] getRow(teamVO t) {
-		String[] temp = new String[28];
-		temp[0] = t.getName();
-		temp[1] = t.getGames()+"";
-		temp[2] = t.getfreeGoalMade()+"";
-		temp[3] = t.getfieldGoalAttempt()+"";
-		temp[4] = t.getNumberOfThreeGoalMade()+"";
-		temp[5] = t.getNumberOfThreeGoalAttempt()+"";
-		temp[6] = t.getFreeThrowMade()+"";
-		temp[7] = t.getFreeThrowAttempt()+"";
-		temp[8] = t.getOffenseRebound()+"";
-		temp[9] = t.getDefenseRebound()+"";
-		temp[10] = t.getRebound()+"";
-		temp[11] = t.getAssist()+"";
-		temp[12] = t.getSteal()+"";
-		temp[13] = t.getBlockShot()+"";
-		temp[14] = t.getError()+"";
-		temp[15] = t.getFoul()+"";
-		temp[16] = t.getScore()+"";
-		temp[17] = t.getFGP()+"";
-		temp[18] = t.getTGP()+"";
-		temp[19] = t.getFTP()+"";
-		temp[20] = t.getWinP()+"";
-		temp[21] = t.getAttackRound()+"";
-		temp[22] = t.getOE()+"";
-		temp[23] = t.getDE()+"";
-		temp[24] = t.getORE()+"";
-		temp[25] = t.getDRE()+"";
-		temp[26] = t.getSE()+"";
-		temp[27] = t.getAE()+"";
-		
-		return temp;
-	}
-	
 	class returnListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			teams t = new teams();
@@ -103,4 +72,42 @@ public class teamDataAnalysis{
 			frame.dispose();
 		}
 	}
+	
+	class MyTableModel extends AbstractTableModel  { 
+		MemberLogic ml=new MemberLogic();
+	    private String[] columnNames ; 
+	    
+	    
+	    private Object[][] data ;  
+	    
+	    public MyTableModel(Object[][] data,String[] columnNames) {
+	        super();
+	        this.data = data;
+	        this.columnNames=columnNames;
+	         
+	    }
+	 
+	    public int getColumnCount() {  
+	      return columnNames.length;  
+	    }  
+	  
+	    public int getRowCount() {  
+	      return data.length;  
+	    }  
+	    
+	    
+	    public String getColumnName(int col) {  
+	      return columnNames[col];  
+	    }  
+	  
+	    public Object getValueAt(int row, int col) {  
+	      return data[row][col];  
+	    }  
+	 
+	    public Class getColumnClass(int c) {  
+	      return getValueAt(0, c).getClass();  
+	    }  
+	    
+	    
+	  }  
 }
